d7ce9ead830f6212fa06e9d9e96636f108401955
Mafatikhul Habibi <mafatikhulhabibi@gmail.com>
2026-06-12 06:41:15 +0700

    Bug Fix

--- app/controllers/docs_controller.js
+++ app/controllers/docs_controller.js
@@ -2,8 +2,10 @@ export default class DocsController {
     async webhook({ view }) {
         return view.render('docs/webhook');
     }
-    async api_message({ view }) {
-        return view.render('docs/api_message');
+    async api_message({ view, request }) {
+        return view.render('docs/api_message', {
+            baseUrl: `${request.protocol()}://${request.host()}`,
+        });
     }
     async api_group({ view }) {
         return view.render('docs/api');
